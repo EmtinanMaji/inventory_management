2e3dae6ea0bc5e26b4ded976e51820d0cf78569b
EmtinanMaji <emtinanmaji@gmail.com>
2024-03-24 15:00:59 +0300

    Class

--- Program.cs
+++ Program.cs
@@ -1,2 +1,55 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Item
+{
+    public string Name {get;}
+    private int Quantity {get; set;}
+    private DateTime CreatedDate {get; set;}
+
+    public Item(string name, int quantity, DateTime createdDate = default){
+        if(quantity < 0){
+            Console.WriteLine("Quantity cannot be negative.");
+            return;
+        }
+        Name = name;
+        Quantity = quantity;
+        CreatedDate = createdDate == default ? DateTime.Now : createdDate;
+    }
+} 
+
+class Store
+{
+    private List<Item> items = new List<Item>();
+
+    public void AddItem(Item item)
+    {
+        
+    }
+    public void DeleteItem(string itemName)
+    {
+    
+    }
+
+    public int GetCurrentVolume()
+    {
+        return;
+    }
+
+    public Item FindItemByName(string itemName)
+    {
+        return;
+    }
+
+    public List<Item> SortByNameAsc()
+    {
+        return;
+    }
+
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+
+    }
+}
